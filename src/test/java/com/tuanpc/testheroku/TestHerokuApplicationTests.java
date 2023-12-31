61796f767799d7aeefd8b34aa5e9d3400178d527
tuanpc1902 <82777525+tuanpc1902@users.noreply.github.com>
2023-12-31 14:03:37 +0700

    initial commit

---
+++ src/test/java/com/tuanpc/testheroku/TestHerokuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuanpc.testheroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestHerokuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
